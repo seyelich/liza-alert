285ad7fe9d8933481276ed585df03aa6126aee2f
Vadim Belousov <thevadiratti@yandex.ru>
2022-04-26 12:23:24 +0500

    fix: исправить баг с переворотом фильтров

--- scripts/courses.js
+++ scripts/courses.js
@@ -2,7 +2,7 @@ const hideButtons = document.querySelectorAll('.filters__hide-button');
 
 hideButtons.forEach(item => {
   item.addEventListener('click', function (event) {
-    if (!event.target.hasAttribute('area-label')) {
+    if (!event.target.hasAttribute('aria-label')) {
       event.target.parentElement.lastElementChild.classList.toggle('filters__hide-icon_opened');
       event.target.parentElement.nextElementSibling.classList.toggle('filters__checkboxes_opened');
     }
